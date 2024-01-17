a1fcb875dc6bc6b3c7de9f361481e269276042a2
Xavier Julies <78542365+XavierJ96@users.noreply.github.com>
2024-01-17 11:44:42 +0200

    intial commit

---
+++ load.sh
@@ -0,0 +1,12 @@
+echo "$(date +"%Y-%m-%d %H:%M:%S") Check if there are changes in the Git repository" >> /var/log/cron.log
+if git fetch origin && ! git diff --quiet main..origin/main; then
+echo "$(date +"%Y-%m-%d %H:%M:%S") Pulling the latest changes from the Git repository" >> /var/log/cron.log
+git pull
+else
+echo "$(date +"%Y-%m-%d %H:%M:%S") No changes in the Git repository" >> /var/log/cron.log
+exit 0
+fi
+
+echo "$(date +"%Y-%m-%d %H:%M:%S") Copy nginx.conf to Nginx configuration">> /var/log/cron.log
+sudo cp nginx/nginx.conf /etc/nginx/conf.d/nginx.conf
+sudo systemctl restart nginx
